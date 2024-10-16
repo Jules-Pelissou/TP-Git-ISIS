0e1b227b85f65c158de5ffe9fb84460bc4a39531
Jules Pelissou <jules.pelissou@etud.univ-jfc.fr>
2024-10-16 08:58:16 +0200

    Updating main

--- src/main/java/org/github/pelissou/Main.java
+++ src/main/java/org/github/pelissou/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+
+        // Salut c'est le commentaire inutile
+        // Commentaire nul v2
+
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
