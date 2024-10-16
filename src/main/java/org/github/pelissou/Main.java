359d6127946e4f2b6c74b040461eac1531ccafe2
Ahmed-BEN-REJEB <ahmed.ben_rejeb@etud.univ-jfc.fr>
2024-10-16 10:30:20 +0200

    j'ai modifie

--- src/main/java/org/github/pelissou/Main.java
+++ src/main/java/org/github/pelissou/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println("*");
+                System.out.println("A");
             }
         }
     }
